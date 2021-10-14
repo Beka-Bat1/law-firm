cafa5d2e4a34facd16642399acab45213b4b8f7c
Beka-bat1 <bekabatmanashili1@gmail.com>
2021-10-14 15:31:50 +0400

    about us page

--- src/pages/AboutUsPage/index.tsx
+++ src/pages/AboutUsPage/index.tsx
@@ -1,21 +1,64 @@
 import React from 'react';
-
+import {FindOutMore} from 'components';
+import AboutCover from '@img/group.jpg';
 import './styles.css';
-
-import Aboutbackground from '@img/group.jpg';
 
 function AboutUsPage() {
    return (
       <>
+         <div
+            className="image-container d-flex"
+            style={{backgroundImage: `url(${AboutCover})`}}>
+            <p className="aboutus-header fs-1 fw-bold text-white mt-auto">
+               Why us?
+            </p>
+         </div>
          <div className="container-fluid">
-            <div className="image-container d-flex">
-               <p className="aboutus-header fs-1 fw-bold text-white mt-auto">
-                  Why us?
-               </p>
-            </div>
+            <div className="container section">
+               <div className="row section">
+                  <div className="col-6 ms-auto">
+                     <p className="fs-3 text-start">
+                        Lorem, ipsum dolor sit amet consectetur adipisicing
+                        elit. Illum magnam itaque earum in alias, exercitationem
+                        ad neque dolorum dolores placeat fuga officiis nemo qui
+                        eligendi eveniet sed esse mollitia laborum.
+                     </p>
+                  </div>
+               </div>
 
-            <div className="container">
-               <p>asdasdsa</p>
+               <div className="row section">
+                  <div className="col-12">
+                     <p className="fs-4 mb-5">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Laborum sequi commodi, temporibus consequatur impedit,
+                        tenetur quia eligendi sed nobis quibusdam reiciendis
+                        illo ullam. Rem labore quibusdam quaerat.
+                     </p>
+                     <p className="fs-4 mb-5">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Laborum sequi commodi, temporibus consequatur impedit,
+                        tenetur quia eligendi sed nobis quibusdam reiciendis
+                        illo ullam. Rem labore quibusdam quaerat.
+                     </p>
+                     <p className="fs-4 mb-5">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Laborum sequi commodi, temporibus consequatur impedit,
+                        tenetur quia eligendi sed nobis quibusdam reiciendis
+                        illo ullam. Rem labore quibusdam quaerat.
+                     </p>
+                  </div>
+               </div>
+               <div className="row ">
+                  <FindOutMore
+                     textLeft={
+                        "If you can't find what you are looking for, explore."
+                     }
+                     buttonText={'Find Out More'}
+                     onClick={() => {
+                        alert('Find Out');
+                     }}
+                  />
+               </div>
             </div>
          </div>
       </>
--- src/pages/ServicesPage/index.tsx
+++ src/pages/ServicesPage/index.tsx
@@ -1,7 +1,143 @@
 import React from 'react';
+import {FindOutMore} from 'components';
+
+import ServicesCover from '@img/services.jpg';
+
+import './styles.css';
 
 function ServicesPage() {
-   return <div></div>;
+   const accordionData = [
+      {
+         headerLeft: 'Banking and Financial Service',
+         practiceCount: 3,
+         text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque, aspernatur possimus. Placeat at eveniet, veniam consequuntur, repellendus officiis magnam expedita qui provident laborum debitis commodi cupiditate alias voluptatum repudiandae quibusdam.',
+      },
+      {
+         headerLeft: 'Banking and Financial Service',
+         practiceCount: 3,
+         text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque, aspernatur possimus. Placeat at eveniet, veniam consequuntur, repellendus officiis magnam expedita qui provident laborum debitis commodi cupiditate alias voluptatum repudiandae quibusdam.',
+      },
+      {
+         headerLeft: 'Banking and Financial Service',
+         practiceCount: 3,
+         text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque, aspernatur possimus. Placeat at eveniet, veniam consequuntur, repellendus officiis magnam expedita qui provident laborum debitis commodi cupiditate alias voluptatum repudiandae quibusdam.',
+      },
+      {
+         headerLeft: 'Banking and Financial Service',
+         text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque, aspernatur possimus. Placeat at eveniet, veniam consequuntur, repellendus officiis magnam expedita qui provident laborum debitis commodi cupiditate alias voluptatum repudiandae quibusdam.',
+      },
+      {
+         headerLeft: 'Banking and Financial Service',
+         practiceCount: 3,
+         text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque, aspernatur possimus. Placeat at eveniet, veniam consequuntur, repellendus officiis magnam expedita qui provident laborum debitis commodi cupiditate alias voluptatum repudiandae quibusdam.',
+      },
+      {
+         headerLeft: 'Banking and Financial Service',
+         practiceCount: 3,
+         text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque, aspernatur possimus. Placeat at eveniet, veniam consequuntur, repellendus officiis magnam expedita qui provident laborum debitis commodi cupiditate alias voluptatum repudiandae quibusdam.',
+      },
+      {
+         headerLeft: 'Banking and Financial Service',
+         practiceCount: 3,
+         text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque, aspernatur possimus. Placeat at eveniet, veniam consequuntur, repellendus officiis magnam expedita qui provident laborum debitis commodi cupiditate alias voluptatum repudiandae quibusdam.',
+      },
+   ];
+
+   return (
+      <>
+         <div
+            className="image-container d-flex"
+            style={{backgroundImage: `url(${ServicesCover})`}}>
+            <p className="aboutus-header fs-1 fw-bold text-white mt-auto">
+               Our Services
+            </p>
+         </div>
+         <div className="container-fluid">
+            <div className="container section">
+               <div className="row section">
+                  <div className="col-6">
+                     <p className="fs-3 text-start">Service</p>
+                  </div>
+                  <div className="col-3">
+                     <p className="fs-3 text-center">Practice</p>
+                  </div>
+                  <div className="col-3">
+                     <p className="fs-3 text-end">Open</p>
+                  </div>
+               </div>
+            </div>
+
+            <div className="container-fluid">
+               <div
+                  className="accordion accordion-flush"
+                  id="servicesAccordion">
+                  {accordionData.map((item, index) => (
+                     <div
+                        className="accordion-item accordion-header container"
+                        data-bs-toggle="collapse"
+                        data-bs-target={`#flush-collapse${index}`}
+                        style={{cursor: 'pointer'}}>
+                        <div className="container">
+                           <div className="row mt-4">
+                              <div className="col-6 p-0">
+                                 <p className="fs-4 ">{item.headerLeft}</p>
+                              </div>
+                              <div className="col-3">
+                                 <p className="fs-4 text-center">
+                                    {item.practiceCount || 0} Practices
+                                 </p>
+                              </div>
+                              <div className="col-3">
+                                 <button
+                                    className="accordion-button collapsed caret-button "
+                                    type="button"
+                                    data-bs-toggle="collapse"
+                                    data-bs-target={`#flush-collapse${index}`}
+                                    aria-expanded="false"
+                                    aria-controls={`flush-collapse${index}`}
+                                    style={{
+                                       boxShadow: 'none',
+                                       background: 'none',
+                                       color: 'black',
+                                       border: 'none',
+                                       font: 'inherit',
+                                       cursor: 'pointer',
+                                       outline: 'inherit',
+                                       textUnderlineOffset: 'false',
+                                       marginLeft: '1em',
+                                    }}
+                                 />
+                              </div>
+                           </div>
+                        </div>
+                        <div
+                           id={`flush-collapse${index}`}
+                           className="accordion-collapse collapse"
+                           aria-labelledby={`flush-heading${index}`}
+                           data-bs-parent="#servicesAccordion">
+                           <div className="accordion-body my-4">
+                              <p className="fs-5">{item.text}</p>
+                           </div>
+                        </div>
+                     </div>
+                  ))}
+               </div>
+            </div>
+
+            <div className="container section">
+               <FindOutMore
+                  textLeft={
+                     "If you can't find what you are looking for, explore."
+                  }
+                  buttonText={'Find Out More'}
+                  onClick={() => {
+                     alert('Find Out');
+                  }}
+               />
+            </div>
+         </div>
+      </>
+   );
 }
 
 export default ServicesPage;
